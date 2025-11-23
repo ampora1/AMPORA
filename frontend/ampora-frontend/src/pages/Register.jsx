4cbb21f333c8b4d10c773cb9cd1679dc89b96a6b
Maheshi Wickramarathna <maheshiwickramarathna2001@gmail.com>
2025-11-23 19:46:46 +0530

    added css

--- frontend/ampora-frontend/src/pages/Register.jsx
+++ frontend/ampora-frontend/src/pages/Register.jsx
@@ -7,7 +7,8 @@ export default function Register() {
 
   return (
     <div className="min-h-screen w-screen flex  flex-row overflow-hidden p-4 justify-center bg-[#EDFEFF] fixed top-0 right-0 left-0">
-     <div className="w-full h-[700px] flex border rounded shadow-xl/30 bg-white">
+     {/* <div className="w-full h-[700px] flex border rounded shadow-xl/30 bg-white"> */}
+     <div className="bg-[#EDFEFF] w-[900px] h-[700px] flex flex-row  shadow-xl/30 overflow-hidden">
       {/* =============Image============= */}
       <div className="w-1/2 h-full ">
         <img
@@ -18,33 +19,33 @@ export default function Register() {
       </div>
 
       {/* =================Form===================== */}
-      <div className="w-1/2 flex  items-center  border ">
-        <form className="w-[380px] border p-4 shadow-2xl">
+      <div className="w-1/2 flex  pl-13 items-center rounded-3xl bg-white">
+        <form className="w-[400px]  h-full  p-4 pl-10">
           
-          <h2 className="text-3xl font-bold mb-2">Sign up</h2>
+          <h2 className="font-inter font-bold text-[36px] text-[#000000]  mb-6">Sign up</h2>
 
           <input
               type="text"
               placeholder="First name"
-              className="w-full border border-gray-400 rounded-md p-3 mb-5 focus:outline-none"
+              className="w-[300px] h-[45px] border border-gray-500 rounded-2xl p-2 mb-5 focus:outline-none"
           />
 
           <input
               type="text"
               placeholder="Last name"
-              className="w-full border border-gray-400 rounded-md p-3 mb-5 focus:outline-none"
+              className="w-[300px]  h-[45px] border border-gray-500 rounded-2xl p-2 mb-5 focus:outline-none"
           />
 
           <input
               type="text"
               placeholder="Phone no"
-              className="w-full border border-gray-400 rounded-md p-3 mb-5 focus:outline-none"
+              className="w-[300px]  h-[45px] border border-gray-500 rounded-2xl p-2 mb-5 focus:outline-none"
           />
           
           <input
             type="email"
             placeholder="Enter your email"
-            className="w-full border border-gray-400 rounded-md p-3 mb-5 focus:outline-none"
+            className="w-[300px]   h-[45px] border border-gray-500 rounded-2xl p-2 mb-5 focus:outline-none"
           />
 
           
@@ -52,17 +53,17 @@ export default function Register() {
           <input
             type={showPassword ? "text" : "password"}
             placeholder="Enter password"
-            className="w-full border border-gray-400 rounded-md p-3 mb-3 focus:outline-none"
+            className="w-[300px]  h-[45px] border border-gray-500 rounded-2xl p-2 mb-3 focus:outline-none"
           />
 
           <input
               type={showPassword ? "text" : "password"}
               placeholder="Confirm password"
-              className="w-full border border-gray-400 rounded-md p-3 mb-5 focus:outline-none"
+              className="w-[300px]  h-[45px] border border-gray-500 rounded-2xl p-2 mb-2 focus:outline-none"
           />
 
           
-          <div className="flex items-center gap-2 mb-6">
+          <div className="font-inter font-regular text-black flex items-center gap-2 mb-8">
             <input
               type="checkbox"
               onChange={() => setShowPassword(!showPassword)}
@@ -70,25 +71,26 @@ export default function Register() {
             <label>Show password</label>
           </div>
           
-          <button className="w-full bg-white text-gray-500 p-3 rounded-md font-semibold mb-6">
-            Sign Up
-          </button>
+          <a className="w-[300px] flex items-center justify-center  p-3  bg-[#0ABAB5] hover:bg-[#56DFCF] rounded-lg  shadow-md" href="">
+            
+            <span className=" text-white font-bold">Sing Up</span>
+          </a>
           
           {/* Divider */}
-          <div className="flex items-center mb-6">
-            <hr className="flex-grow border-gray-300" />
-            <span className="mx-3 text-gray-500">or</span>
-            <hr className="flex-grow border-gray-300" />
+          <div className="flex items-center mb-8 w-[200px]  ml-12 ">
+            <hr className="flex-grow border-black" />
+            <span className="mx-3 text-black">or</span>
+            <hr className="flex-grow border-black" />
           </div>
 
           {/* Google Button */}
-          <a className="w-full flex items-center justify-center gap-3 border border-gray-800 py-3 rounded-md bg-gray-300  hover:bg-[#505252] " href="">
+          <a className="w-[300px]   flex items-center justify-center gap-2 p-3  bg-black hover:bg-[#505252]  rounded-full shadow-md" href="">
             <img
               src="https://www.svgrepo.com/show/355037/google.svg"
-              className="w-6"
+              className="w-4"
               alt="Google"
             />
-            Continue With Google
+            <span className=" text-white font-bold">Continue With Google</span>
           </a>
           </form>
         </div>
